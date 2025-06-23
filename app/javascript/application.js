c79d114226334c92bea133a703b77ceafdcbc17f
MatEstevam <matheus.estevam.br@gmail.com>
2025-06-23 12:36:47 -0300

    att refresh buttom

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,10 @@
 import { Application } from "@hotwired/stimulus"
 import CarouselController from "./controllers/carousel_controller"
 import ContadorController from "./controllers/contador_controller"
+import FraseController from "./controllers/frase_controller"
 
 
 const application = Application.start()
 application.register("carousel", CarouselController)
 application.register("contador", ContadorController)
+application.register("frase", FraseController)
